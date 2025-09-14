129662e89cd71c9725558d40084d52619d2577b1
dante18 <lablandinerie2@gmail.com>
2025-09-14 14:43:45 +0200

    Rendu final: Add files used to manage monsters

---
+++ TpGameAdmin/Presentation/ConsoleMenu.cs
@@ -0,0 +1,59 @@
+﻿using TpGameCore.Model;
+using TpGameCore.Presentation;
+
+namespace TpGameAdmin.Presentation;
+
+public class ConsoleMenu
+{
+    public void AfficherMenuPrincipal()
+    {
+        Console.WriteLine("1 - Lister les monstres");
+        Console.WriteLine("2 - Chercher un monstre");
+        Console.WriteLine("3 - Créer un monstre");
+        Console.WriteLine("4 - Mettre à jour un monstre");
+        Console.WriteLine("5 - Supprimer un monstre");
+        Console.WriteLine("6 - Quitter");
+        Console.WriteLine("");
+    }
+
+    public MonstreModel GestionMenuAjouterMonstre()
+    {
+        var nom = AppEntry.GetStringEntry("Saisissez le nom du monstre : ");
+
+        if (string.IsNullOrWhiteSpace(nom))
+        {
+            Random rnd = new Random();
+            nom = "monstre" + rnd.Next(1, 100);
+        }
+
+        var hp = AppEntry.GetIntegerEntry("Saisissez les HP du monstre : ");
+        var attaque = AppEntry.GetIntegerEntry("Saisissez l'attaque du monstre : ");
+
+        return new MonstreModel(nom, hp, attaque);
+    }
+
+    public MonstreModel GestionMenuMiseAjourMonstre(MonstreModel monstre)
+    {
+        Console.WriteLine("Quel champ voulez-vous modifier ?");
+        Console.WriteLine("1 - Nom");
+        Console.WriteLine("2 - Point de vie");
+        Console.WriteLine("3 - Attaque");
+        Console.WriteLine("4 - Quitter");
+        int choix = AppEntry.GetIntegerEntry("\n");
+
+        if (choix == 1)
+        {
+            monstre.nom = AppEntry.GetStringEntry("Quel sera le nouveau nom du monstre ?");
+        }
+        else if (choix == 2)
+        {
+            monstre.pointDeVie = AppEntry.GetIntegerEntry("Quel sera les nouveaux point de vie ?");
+        }
+        else if (choix == 2)
+        {
+            monstre.attaque = AppEntry.GetIntegerEntry("Quel sera les nouveaux point d'attaque ?");
+        }
+
+        return monstre;
+    }
+}
--- TpGameAdmin/Program.cs
+++ TpGameAdmin/Program.cs
@@ -1,2 +1,140 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TpGameAdmin.Presentation;
+using TpGameCore.Context;
+using TpGameCore.Model;
+using TpGameCore.Presentation;
+using TpGameCore.Repository;
+
+Console.WriteLine("Bienvenue dans la gestion des monstres");
+Console.WriteLine("");
+
+int choix;
+ConsoleMenu consoleMenu = new ConsoleMenu();
+
+using var context = new TpGameContext();
+MonstreRepository repository = new MonstreRepository(context);
+int nombreMonstre = repository.ObtenirNombreDeMonstre();
+
+do
+{
+    consoleMenu.AfficherMenuPrincipal();
+
+    choix = AppEntry.GetIntegerEntry("Que souhaitez-vous faire ?");
+    Console.Clear();
+    Console.WriteLine("");
+
+    switch (choix)
+    {
+        case 1:
+            if (nombreMonstre > 0)
+            {
+                List<MonstreModel> monstres = repository.ObtenirListMonstre();
+
+                foreach (var monstre in monstres)
+                {
+                    Console.WriteLine(monstre);
+                }
+
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Aucun monstre");
+                Console.WriteLine("");
+            }
+            break;
+        case 2:
+            if (nombreMonstre > 0)
+            {
+                int choixMonstre = AppEntry.GetIntegerEntry("Quel monstre voulez-vous regarder ?");
+                MonstreModel monstreAConsulter = repository.ObtenirMonstre(choixMonstre);
+                Console.WriteLine("");
+
+                if (monstreAConsulter is null)
+                {
+                    Console.WriteLine("Le monstre n'existe pas");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine(monstreAConsulter);
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aucun monstre");
+                Console.WriteLine("");
+            }
+            break;
+        case 3:
+            repository.AjouterMonstre(consoleMenu.GestionMenuAjouterMonstre());
+            Console.WriteLine("");
+            nombreMonstre = repository.ObtenirNombreDeMonstre();
+            break;
+        case 4:
+            if (nombreMonstre > 0)
+            {
+                int choixMonstreAMettreAJour = AppEntry.GetIntegerEntry("Quel monstre voulez-vous modifier ?");
+                MonstreModel monstreAMettreAJour = repository.ObtenirMonstre(choixMonstreAMettreAJour);
+                Console.WriteLine("");
+
+                if (monstreAMettreAJour is null)
+                {
+                    Console.WriteLine("Le monstre n'existe pas");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    monstreAMettreAJour = consoleMenu.GestionMenuMiseAjourMonstre(monstreAMettreAJour);
+                    Console.WriteLine("");
+
+                    bool monstreAMettreAJourResultat = repository.MiseAJourMonstre(choixMonstreAMettreAJour, monstreAMettreAJour);
+
+                    if (monstreAMettreAJourResultat)
+                    {
+                        Console.WriteLine("Le monstre a été mis à jour");
+                    }
+
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aucun monstre");
+                Console.WriteLine("");
+            }
+            break;
+        case 5:
+            if (nombreMonstre > 0)
+            {
+                int choixMonstreASupprimer = AppEntry.GetIntegerEntry("Quel monstre voulez-vous supprimer ?");
+                MonstreModel monstreASupprimer = repository.ObtenirMonstre(choixMonstreASupprimer);
+                Console.WriteLine("");
+
+                if (monstreASupprimer is null)
+                {
+                    Console.WriteLine("Le monstre n'existe pas");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    bool monstreASupprimerResultat = repository.SupprimerMonstre(choixMonstreASupprimer);
+
+                    if (monstreASupprimerResultat)
+                    {
+                        Console.WriteLine("Le monstre a été supprimé");
+                    }
+
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aucun monstre");
+                Console.WriteLine("");
+            }
+            break;
+        case 6:
+            break;
+    }
+} while (choix != 6);
